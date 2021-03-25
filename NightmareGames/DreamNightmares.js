376c4760881453e83838a70b98db1298a0160735
gabi99-cloud <80900090+gabi99-cloud@users.noreply.github.com>
2021-03-25 12:49:27 -0500

    Add files via upload

---
+++ NightmareGames/DreamNightmares.js
@@ -0,0 +1,44 @@
+{
+    var name = window.prompt("entername")
+function promptentername)
+let entername = prompt! ""
+if prompt === "!entername";
+alert function "entername";
+    
+}
+
+var ("Dream 1") = (on click)
+function <button class="btn">Dream 1</button>
+let ("Dream 1")=prompt ("You have entered the nightmare moonworld 1 and must fight the dreamcatcher in order to escape back to your peaceful dream");
+if ("Dream1" === "") {
+alert ("choose challenge or exit");
+break;
+
+var ("Dream 2") = (on click)
+function <button class="btn">Dream 2</button>
+let ("Dream 2")=prompt (
+if ("Dream 2" === "") 
+switch ("Dream 3");
+let ("Dream 3") = (prompt = "sudden death challenge");
+
+
+ switch ("Dream 2") {
+     case 1: "user chooses to accept challenge in order to get back to peaceful dream, must choose between a:Underworld b:Waterworld c:Saturns Rings")
+         break;
+ 
+     default:
+         break;
+ }
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+case A : 
+break;
+
+
+}
+
